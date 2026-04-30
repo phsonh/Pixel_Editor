b4cf65b32aa12f174761feb217ea38b4eac5f717
Lonely Owl <benhuangxianan@gmail.com>
2026-04-30 17:27:08 +0800

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,1 @@
-@echo off
-echo 正在使用Nuitka编译
-python -m venv venv_build && call .\venv_build\Scripts\activate && python -m pip install --upgrade pip && pip install PyQt5 nuitka zstandard && python -m nuitka --standalone --onefile --windows-console-mode=disable --enable-plugin=pyqt5 --remove-output --output-dir=dist main.py && echo 编译完成 && pause
+pyinstaller -F -w --hidden-import=PyQt5.sip main.py
